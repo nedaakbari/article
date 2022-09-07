7494d5f8b399f92f1036661d109944b1858a7b99
nedaa <neda>
2022-09-07 21:21:10 +0430

    base class don

---
+++ src/main/java/ir/maktab/model/Article.java
@@ -0,0 +1,94 @@
+package ir.maktab.model;
+
+import java.sql.Date;
+
+public class Article {
+    private int id;
+    private String title;
+    private String brief;
+    private String content;
+    private Date createDate;
+    private boolean isPublished;
+    private int userID;
+
+
+    public Article(int id, String title, String brief, String content, Date createDate, boolean isPublished, int userID) {
+        this.id = id;
+        this.title = title;
+        this.brief = brief;
+        this.content = content;
+        this.createDate = createDate;
+        this.isPublished = isPublished;
+        this.userID = userID;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBrief() {
+        return brief;
+    }
+
+    public void setBrief(String brief) {
+        this.brief = brief;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public boolean isPublished() {
+        return isPublished;
+    }
+
+    public void setPublished(boolean published) {
+        isPublished = published;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Article{" +
+                "title='" + title + '\'' +
+                ", brief='" + brief + '\'' +
+                ", content='" + content + '\'' +
+                ", createDate=" + createDate +
+                ", isPublished=" + isPublished +
+                ", userID=" + userID +
+                '}';
+    }
+
+}
---
+++ src/main/java/ir/maktab/model/User.java
@@ -0,0 +1,73 @@
+package ir.maktab.model;
+
+import java.sql.Date;
+
+public class User {
+
+    private int id;
+    private String username;
+    private String nationalCode;
+    private Date birthday;
+    private String password;
+
+
+    public User(int id, String username, String nationalCode, Date birthday, String password) {
+        this.id = id;
+        this.username = username;
+        this.nationalCode = nationalCode;
+        this.birthday = birthday;
+        this.password = password;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getNationalCode() {
+        return nationalCode;
+    }
+
+    public void setNationalCode(String nationalCode) {
+        this.nationalCode = nationalCode;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", nationalCode='" + nationalCode + '\'' +
+                ", birthday=" + birthday +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
